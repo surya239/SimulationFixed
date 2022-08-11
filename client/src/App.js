091726bf70f3938e8f7c70df3fd5fb4d3cff4107
Surya <suryab.2392000@gmail.com>
2022-08-11 16:11:29 +0530

    cd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Signup from './Pages/Signup'
-import Login from './Pages/Login';
+
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import { PrivateRoute } from './Auth/PrivateRoute';
 
