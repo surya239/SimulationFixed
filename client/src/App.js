73ade0e8a364e507ee3308346241eaee08574500
Surya <suryab.2392000@gmail.com>
2022-08-11 12:41:46 +0530

    c

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,7 @@ import Signup from './Pages/Signup'
 import Login from './Pages/Login';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import { PrivateRoute } from './Auth/PrivateRoute';
-import Profile from './Pages/ProfileUpdation'
-import UpdateStudent from './Pages/Studentupdate'
-import StudentDashboard from './Pages/StudentDashboard';
-import Alumini from './Pages/Alumini';
-import Loding from './Pages/Loding'
-import PostJob from './Pages/PostJob';
-import EditProfile from './Pages/EditProfile';
-import ViewApplication from './Pages/ViewApplicants';
+
 import Dashboard from './Pages/Dashboard';
 function App() {
   return (
