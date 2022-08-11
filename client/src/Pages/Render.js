49143cbf5e930c4781276983f48e5072d50a854b
Surya <suryab.2392000@gmail.com>
2022-08-11 16:24:51 +0530

    c

--- client/src/Pages/Render.js
+++ client/src/Pages/Render.js
@@ -25,44 +25,44 @@ function Render(params){
             </> )
     }
 
-    else if(page === 'Effort'){
-        return(
-            <>
-                <Effort />
-                <Productivity />
-            </>
-        )
-    }
+    // else if(page === 'Effort'){
+    //     return(
+    //         <>
+    //             <Effort />
+    //             <Productivity />
+    //         </>
+    //     )
+    // }
 
-    else if(page === 'Productivity'){
-        return <Productivity />
-    }
+    // else if(page === 'Productivity'){
+    //     return <Productivity />
+    // }
 
-    else if(page === 'Resources'){
-        return <Resource />
-    }
+    // else if(page === 'Resources'){
+    //     return <Resource />
+    // }
 
-    else if(page === 'ResourceCost'){
-        return <ResourceCost />
-    }
-    else if(page === 'Project'){
-        return <Project />
-    }
-    else if(page === 'subContract'){
-        return <Subcontract />
-    }
-    else if(page === 'Infra'){
-        return <Infra />
-    }
-    else if(page === 'costOfCapital'){
-        return <CostofCapital />
-    }
-    else if(page === 'Contigency'){
-        return <SelectContigency />
-    }
-    else if(page === 'Summary'){
-        return <Summary />
-    }
+    // else if(page === 'ResourceCost'){
+    //     return <ResourceCost />
+    // }
+    // else if(page === 'Project'){
+    //     return <Project />
+    // }
+    // else if(page === 'subContract'){
+    //     return <Subcontract />
+    // }
+    // else if(page === 'Infra'){
+    //     return <Infra />
+    // }
+    // else if(page === 'costOfCapital'){
+    //     return <CostofCapital />
+    // }
+    // else if(page === 'Contigency'){
+    //     return <SelectContigency />
+    // }
+    // else if(page === 'Summary'){
+    //     return <Summary />
+    // }
 }
 
 export default Render
